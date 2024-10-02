ab59c5376416a3e95230b132183f2ff6d9270710
jack-sparrow-18-04 <akhmedovkhasanboy28@gmail.com>
2024-10-02 12:42:29 +0500

    first commit

---
+++ src/main/java/uz/simple/simplewebproject/SimpleWebProjectApplication.java
@@ -0,0 +1,13 @@
+package uz.simple.simplewebproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleWebProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/simple/simplewebproject/SimpleWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.simple.simplewebproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
